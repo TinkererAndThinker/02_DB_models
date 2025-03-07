80deacf723d098e70f688444c572879e8a768624
TinkererAndThinker <scribbling.researcher@gmail.com>
2025-03-07 12:37:35 +0100

    ++++++

--- kardinalitaet_1_n/03_1-n.sql
+++ kardinalitaet_1_n/03_1-n.sql
@@ -2,27 +2,53 @@
 -- Vorbereitung
 
 
+DROP TABLE IF EXISTS design.kittens;
+DROP TABLE IF EXISTS design.catmoms;
 
--- MT: catmoms
-
+-- MT: cats
+CREATE TABLE IF NOT EXISTS design.catmoms
+(
+  id        INT         NOT NULL AUTO_INCREMENT,
+  cat_name  VARCHAR(45) NOT NULL,
+  fur_color VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 -- MT: Struktur
-
+DESCRIBE design.catmoms;
 
 -- MT: Inserts
-
+INSERT INTO catmoms (id, cat_name, fur_color) VALUES (DEFAULT, "Grizabella", "white");
+INSERT INTO catmoms (id, cat_name, fur_color) VALUES (DEFAULT, "Mausi", "striped");
 
 -- MT: Inhalte
+SELECT * FROM design.catmoms;
 
+-- DT:design.kittens
+CREATE TABLE IF NOT EXISTS design.kittens
+(
+  id          INT         NOT NULL AUTO_INCREMENT,
+  kitten_name VARCHAR(45) NOT NULL,
+  fur_color   VARCHAR(45) NOT NULL,
+  catmoms_id  INT         NOT NULL,
+  PRIMARY KEY (id)
+);
 
--- DT: kittens
-
+ALTER TABLE design.kittens
+  ADD CONSTRAINT FK_catmoms_TO_kittens
+    FOREIGN KEY (catmoms_id)
+    REFERENCES catmoms (id);
 
 -- DT: Struktur
-
+DESCRIBE design.kittens;
 
 -- DT: Inserts
-
+INSERT INTO design.kittens (id, kitten_name, fur_color, catmoms_id) VALUES (DEFAULT, "Grizzi_1", "white", 1);
+INSERT INTO design.kittens (id, kitten_name, fur_color, catmoms_id) VALUES (DEFAULT, "Grizzi_2", "white", 1);
+INSERT INTO design.kittens (id, kitten_name, fur_color, catmoms_id) VALUES (DEFAULT, "Mausini", "striped", 2);
 
 -- DT: Inhalte
+SELECT * FROM design.kittens;
+
+
 
