eae9c62e15bd11a158621fe61f6a36aee1e5b31f
TinkererAndThinker <scribbling.researcher@gmail.com>
2025-03-07 10:49:56 +0100

    +++

--- kardinalitaet_1_1/03_1-1.sql
+++ kardinalitaet_1_1/03_1-1.sql
@@ -2,15 +2,28 @@
 DROP Table IF EXISTS design.servants;
 DROP Table IF EXISTS design.cats;
 
-
-
 -- Mastertabelle (MT): unverändert
 
+CREATE TABLE IF NOT EXISTS design.cats
+(
+  id        INT         NOT NULL AUTO_INCREMENT COMMENT 'Index',
+  cat_name  VARCHAR(45) NOT NULL COMMENT 'Name',
+  fur_color VARCHAR(45) NOT NULL COMMENT 'FellFarbe',
+  PRIMARY KEY (id)
+) COMMENT 'Katzen';
 
 -- Struktur: MT
 
+DESCRIBE design.cats;
 
 -- Inserts: MT 
+
+INSERT INTO design.cats (id, cat_name,fur_color) VALUES 
+(DEFAULT, "Grizabella", "white"),
+(DEFAULT, "Alonzo", "grey"),
+(DEFAULT, "Mausi", "striped")
+;
+
 
 
 -- Inhalte: MT
@@ -19,17 +32,40 @@ DROP Table IF EXISTS design.cats;
 
 -- Detailtabelle: Verbindung zur MT über Fremdschlüssel
 
+CREATE TABLE IF NOT EXISTS design.servants
+(
+  id           INT           NOT NULL AUTO_INCREMENT COMMENT 'index',
+  servant_name VARBINARY(45) NOT NULL COMMENT 'DienerName',
+  yrs_served   TINYINT       NOT NULL COMMENT 'DienstJahre',
+  id           INT           NOT NULL COMMENT 'Fremdschlüssel',
+  PRIMARY KEY (id)
+) COMMENT 'Diener';
+
 
 -- Fremdschlüssel: DT
-
+ALTER TABLE design.servants
+  ADD CONSTRAINT FK_cats_TO_servants
+    FOREIGN KEY (id)
+    REFERENCES cats (id);
 
 -- wichtig bei 1:1  UNIQUE im fk
+ALTER TABLE design.servants
+  ADD CONSTRAINT UQ_id UNIQUE (id);
 
 
 -- Struktur: DT
 
+DESCRIBE design.servants;
 
 -- Inserts: DT
 
+INSERT INTO design.servants (id, servant_name, yrs_served, cats_id) VALUES 
+(DEFAULT, "Alfred", 5, 1),
+(DEFAULT, "James", 2, 2),
+(DEFAULT, "Jane", 10, 3)
+;
+
 
 -- Inhalte: DT
+
+SELECT * FROM design.servants;
