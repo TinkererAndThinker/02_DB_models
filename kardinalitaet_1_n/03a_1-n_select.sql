7ba8fc74b900dc77a9abea4350f06619e2f1e627
TinkererAndThinker <scribbling.researcher@gmail.com>
2025-03-10 11:59:13 +0100

    --

--- kardinalitaet_1_n/03a_1-n_select.sql
+++ kardinalitaet_1_n/03a_1-n_select.sql
@@ -2,11 +2,9 @@
 -- 04 SELECTS
 
 -- Einzeltabellen
-SELECT * FROM design.cats;
-SELECT * FROM design.servants;
+
 
 -- Inner Join 1 / Gesamte Kombi-Tabelle
-
 
 -- Inner Join 2 / Verwandschaft
 -- "X ist die Mutter von Y"
