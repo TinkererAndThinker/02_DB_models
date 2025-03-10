9fb91edbc41ff22225d82c86cade7077b160124f
TinkererAndThinker <scribbling.researcher@gmail.com>
2025-03-10 21:30:12 +0100

    ++

--- kardinalitaet_1_1/03a_1-1_select.sql
+++ kardinalitaet_1_1/03a_1-1_select.sql
@@ -1,20 +1,44 @@
 
 -- SELECTS
+-- SELECTS
 
 -- Einzeltabellen
-
+SELECT * FROM design.cats;
+SELECT * FROM design.servants;
 
 -- Kreuzprodukt (Kartesisches Produkt)
-
+SELECT * FROM design.cats JOIN design.servants;
 
 -- Inner Join 1 / Gesamte Tabelle
-
+SELECT
+    *
+FROM design.cats INNER JOIN design.servants
+ON design.cats.id = design.servants.cats_id
+;
 
 
 -- Inner Join 2 / (Wer dient wem?)
 -- Wer dient Grizabella?
 -- Wem dient X?
+SELECT
+    cat_name AS Katze,
+    servant_name AS Diener
+FROM design.cats INNER JOIN design.servants
+ON design.cats.id = design.servants.cats_id
+-- Filtern mit WHERE
+-- WHERE cat_name = "Grizabella"
+WHERE servant_name = "Arian"
+;
 
+
+SELECT
+    CONCAT(servant_name, " ist der Diener von ", cat_name) AS Dienstverhaeltnis
+FROM design.cats INNER JOIN design.servants
+ON design.cats.id = design.servants.cats_id
+-- Filtern mit WHERE
+-- WHERE cat_name = "Grizabella"
+WHERE servant_name = "Hubert"
+;
 
 
 
