b411b6ee64786f2f9b58f45796b79aa62139d37f
TinkererAndThinker <scribbling.researcher@gmail.com>
2025-03-11 10:10:06 +0100

    aktuali

--- kardinalitaet_n_m_II/03_1-1_n-m.sql
+++ kardinalitaet_n_m_II/03_1-1_n-m.sql
@@ -1,70 +1,119 @@
 
-# Vorbereitung
-
+-- Vorbereitung
+USE design;
+DROP TABLE IF EXISTS purchases;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS servants;
+DROP TABLE IF EXISTS cats;
 
 /* Cats */
 
-# Mastertabelle: unverändert
+-- Mastertabelle: unverändert
+CREATE TABLE IF NOT EXISTS cats
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  cat_name  VARCHAR(45) NOT NULL,
+  fur_color VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
 
-# Struktur: MT
+-- Struktur: MT
+DESCRIBE design.cats;
 
-# Inserts: MT (Mastertable)
+-- Inserts: MT (Mastertable)
+INSERT INTO cats (id, cat_name,fur_color) VALUES (DEFAULT, "Grizabella", "white");
+INSERT INTO cats (id, cat_name,fur_color) VALUES (DEFAULT, "Alonzo", "grey");
+INSERT INTO cats (id, cat_name,fur_color) VALUES (DEFAULT, "Mausi", "striped");
 
 
 /* SERVANTS */
 
-# Detailtabelle: Verbindung zur MT über Fremdschlüssel
+-- Detailtabelle: Verbindung zur MT über Fremdschlüssel
+CREATE TABLE IF NOT EXISTS servants
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  servant_name VARCHAR(45) NOT NULL,
+  yrs_served   TINYINT NOT NULL,
+  cats_id      INT     NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Fremdschlüssel: DT
+ALTER TABLE servants
+  ADD CONSTRAINT FK_cats_TO_servants
+    FOREIGN KEY (cats_id)
+    REFERENCES cats (id);
+
+-- wichtig bei 1:1  UNIQUE im fk
+ALTER TABLE servants
+  ADD CONSTRAINT UQ_cats_id UNIQUE (cats_id);
+
+-- Struktur: DT
+DESCRIBE design.servants;
+
+-- Inserts: DT
+INSERT INTO servants (id, servant_name, yrs_served, cats_id) VALUES (DEFAULT, "Hasan", 5, 1);
+INSERT INTO servants (id, servant_name, yrs_served, cats_id) VALUES (DEFAULT, "Dimitrij", 2, 2);
+INSERT INTO servants (id, servant_name, yrs_served, cats_id) VALUES (DEFAULT, "Benjamin", 4, 3);
 
 
-
-
-# Fremdschlüssel: DT
-
-
-
-
-# wichtig bei 1:1  UNIQUE im fk
-
-
-
-# Struktur: DT
-
-# Inserts: DT
-
-
-# Servants: Struktur
+-- Servants: Struktur
+DESCRIBE design.servants;
 
 
 /*  PRODUCTS */
 
-# Products: ohne Fremdschlüssel
+-- Products: ohne Fremdschlüssel
+CREATE TABLE IF NOT EXISTS products
+(
+  id            INT          NOT NULL AUTO_INCREMENT,
+  product_name  VARCHAR(45)  NOT NULL,
+  product_price DECIMAL(4,2) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+-- Products: Struktur
+DESCRIBE design.products;
 
+-- Products: Inserts
+INSERT INTO products (id, product_name, product_price) VALUES (DEFAULT, "Whiskas|Lachs", 2.75);
+INSERT INTO products (id, product_name, product_price) VALUES (DEFAULT, "Whiskas|Huhn", 2.80);
+INSERT INTO products (id, product_name, product_price) VALUES (DEFAULT, "Felix|Jelly", 3.75);
+INSERT INTO products (id, product_name, product_price) VALUES (DEFAULT, "Felix|Sauce", 3.80);
 
-
-# Products: Struktur
-
-
-
-# Products: Inserts
-
-
-
-
-
-# Products: Inhalte 
-
-
-
+-- Products: Inhalte 
+SELECT * FROM design.products;
 
 /*  PURCHASES (Kaufprozesse)*/
 
+-- ServantsProducts (purchases)
+CREATE TABLE IF NOT EXISTS purchases
+(
+  servants_id INT NOT NULL,
+  products_id INT NOT NULL
+);
+
+ALTER TABLE purchases
+  ADD CONSTRAINT FK_servants_TO_purchases
+    FOREIGN KEY (servants_id)
+    REFERENCES servants (id);
+
+ALTER TABLE purchases
+  ADD CONSTRAINT FK_products_TO_purchases
+    FOREIGN KEY (products_id)
+    REFERENCES products (id);
+
+-- Purchases: Struktur
+DESCRIBE design.purchases;
+
+-- Purchases: Inserts (Kaufprozesse : Käufer - Produkt)
+INSERT INTO purchases (servants_id, products_id) VALUES (1, 1);
+INSERT INTO purchases (servants_id, products_id) VALUES (1, 3);
+INSERT INTO purchases (servants_id, products_id) VALUES (2, 2);
+INSERT INTO purchases (servants_id, products_id) VALUES (2, 3);
+INSERT INTO purchases (servants_id, products_id) VALUES (2, 4);
+INSERT INTO purchases (servants_id, products_id) VALUES (3, 4);
 
 
-# ServantsProducts (purchases)
-
-# Purchases: Struktur
-
-# Purchases: Inserts (Kaufprozesse : Käufer - Produkt)
-
-# Purchases: Inhalte 
+-- Purchases: Inhalte 
+SELECT * FROM design.purchases;
--- kardinalitaet_n_m_II/03a_1-1_n-m_select.sql
+++ kardinalitaet_n_m_II/03a_1-1_n-m_select.sql
@@ -1,25 +1,62 @@
-# 3b SELECTS 1:1 / n:m
+-- 3b SELECTS 1:1 / n:m
 
-# 1. JOIN 
+-- 1. JOIN 
+SELECT
+	*
+FROM design.purchases
+INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+INNER JOIN design.products ON design.products.id = design.purchases.products_id
+INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+;
 
+-- 2. Wer bekommt den Lachs? 
+SELECT
+	cat_name AS Herrschaft,
+    servant_name AS Diener,
+    product_name AS Produkt
+FROM design.purchases
+INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+INNER JOIN design.products ON design.products.id = design.purchases.products_id
+INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+;
 
+-- 2a. Wer bekommt den Lachs? 
+SELECT
+	concat(
+			servant_name, 
+            " ist der Diener von ", 
+            cat_name,", er kauft ",
+            product_name, 
+            "- somit bekommt ",
+            cat_name, " den Lachs."
+            ) 
+            AS "Wer bekommt den Lachs?"
+FROM design.purchases
+INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+INNER JOIN design.products ON design.products.id = design.purchases.products_id
+INNER JOIN design.cats ON design.cats.id = design.servants.cats_id
+WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+;
 
+-- 2b. Variante mit einer VIEW
+DROP VIEW IF EXISTS design.who_purchased_salmon;
+CREATE VIEW  design.who_purchased_salmon AS
+	SELECT
+		servant_name AS Diener,
+		cats_id
+	FROM design.purchases
+	INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+	INNER JOIN design.products ON design.products.id = design.purchases.products_id
+	WHERE product_name LIKE "%Lachs%" -- irgendwas mit Lachs...
+;
 
+SELECT * FROM design.who_purchased_salmon;
 
-# 2. Wer bekommt den Lachs? 
-
-
-
-
-# 2a. Wer bekommt den Lachs? 
-
-
-
-
-
-# 2b. Variante mit einer VIEW
-
-
+SELECT
+	 concat(cat_name, " bekommt den Lachs.") AS Gourmet
+FROM design.cats INNER JOIN design.who_purchased_salmon  --  VIEW wird als Tabelle behandelt
+ON design.cats.id = design.who_purchased_salmon.cats_id;
 
 
 
